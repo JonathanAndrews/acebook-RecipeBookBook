1bfb2cf38028c76c3567af32d2fa1fb294cdf163
James Rodney <jlrodney95@gmail.com>
2018-10-23 14:19:49 +0100

    first test passing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -27,9 +30,23 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['gmail_username'],
+ :password             => ENV['gmail_password'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
