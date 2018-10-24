6857e7725ac0b35d4edd32b672094fbeb80a0f33
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-24 12:22:59 +0100

    when user logs out, directed to login page

---
+++ spec/features/user_can_log_out_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Logout', type: :feature do
+  scenario 'User can Logout' do
+    @user = build(:user)
+    @user.confirm
+    visit '/users/sign_in'
+    fill_in 'user_email', with: 'joe3@gmail.com'
+    fill_in 'user_password', with: 'blabla'
+    click_button 'Log in'
+    click_link 'Logout'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+end
