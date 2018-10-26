d5528c9442c4aece0172ed54ddc55697b4e80bd5
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-26 09:16:49 +0100

    Removed Commenter from database Comments table
    - redundant as we already had a user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024141609) do
+ActiveRecord::Schema.define(version: 20181025170156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.bigint "post_id"
     t.bigint "user_id"
