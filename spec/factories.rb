e36d6c9fba280768d25bc0c61893f32e8f09ba42
James Rodney <jlrodney95@gmail.com>
2018-10-26 16:41:36 +0100

    Remove ignored files

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
-  factory :comment do
-    commenter { "MyString" }
-    body { "MyText" }
-    post { nil }
-    user { nil }
-  end
-
+  
   factory :user do
     email { 'joe3@gmail.com' }
     password { 'blabla' }
--- spec/features/user_can_see_comment_owner_spec.rb
+++ spec/features/user_can_see_comment_owner_spec.rb
@@ -3,6 +3,22 @@
 require 'rails_helper'
 
 RSpec.feature 'Comments feature test', type: :feature do
+
+  scenario 'should find a back to posts link' do
+    @user = build(:user)
+    @user.confirm
+    visit '/users/sign_in'
+    fill_in 'user_email', with: @user.email
+    fill_in 'user_password', with: @user.password
+    click_button 'Log in'
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link 'Add comment'
+    expect(page).to have_link("Back to posts")
+  end
+
   scenario 'Comment author displayed next to their comment' do
     @user = build(:user)
     @user.confirm
