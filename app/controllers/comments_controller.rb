4e226c0c2cdc61847d2f8e26696417fd0cd66715
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-26 09:10:02 +0100

    Comment Author's name is shown next to their comment
    - Added a feature test to test this.
    - Changed post index view to display comment Author next to Comment
    - added commenter_id to database, then removed it once we realised it was redundant.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create 
     post_id = params[:post_id]
     @post = Post.find(post_id)
-    @comment = @post.comments.create(comment_params.merge(user_id: current_user.id, post_id: post_id ))
+    @comment = @post.comments.create(comment_params.merge(user_id: current_user.id, post_id: post_id))
     redirect_to posts_url
   end 
 
