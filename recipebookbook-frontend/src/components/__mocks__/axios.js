59dd7a2b98abb080e5dd6fd1a0ad2d6d8636294f
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-31 13:51:53 +0000

    testing front end
    - setup jest and enzyme
     - test homepage
     - created mockfetch
    
    Co-authored-by: James Rodney <jlrodney95@gmail.com>

---
+++ recipebookbook-frontend/src/components/__mocks__/axios.js
@@ -0,0 +1,45 @@
+'use strict';
+let currentId = 2;
+
+module.exports = {
+  get: (url) => {
+    return Promise.resolve({
+      data: [
+        {
+          id: 1,
+          message: "message",
+          created_at: "2018-10-26T09:59:53.234Z",
+          updated_at: "2018-10-26T09:59:53.234Z",
+          user_id: 34,
+          user: {
+            email: "jon@urry.me"
+          },
+          comments: [
+            {
+              id: 1,
+              body: "comment 1 by abc",
+              post_id: 1,
+              user_id: 35,
+              created_at: "2018-10-29T11:17:14.662Z",
+              updated_at: "2018-10-29T11:17:14.662Z",
+              user: {
+                email: "a@b.com"
+           }
+         }
+        ]
+       }
+      ]
+    });
+  },
+
+  post: (url, data) => {
+    return Promise.resolve({
+      data: {
+        post: {
+          message: data.post,
+          id: currentId++
+        }
+      }
+    });
+  }
+};
---
+++ recipebookbook-frontend/src/components/__tests__/MyComponent.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import  App from '../../App.js';
+import axios from 'axios';
+import PostContainer from '../PostsContainer.js'
+
+describe('should render title', () => {
+
+  it('should render correctly with no props', () => {
+    const component = shallow(<App/>);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have title', () => {
+    const component = shallow(<App/>);
+    var node = component.find('h1')
+    expect(node.hasClass('App-title')).toBeTruthy();
+    expect(node.html()).toEqual('<h1 class="App-title">RecipeBookBook!</h1>')
+  });
+})
+
+describe('should render posts', () => {
+  it('should fetch a post', () => {
+    const getSpy = jest.spyOn(axios, 'get');
+    const postInstance = shallow(
+      <PostContainer/>
+    );
+    expect(getSpy).toBeCalled();
+  })
+})
