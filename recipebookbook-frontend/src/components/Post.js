1207e1eb15a9c4604f88230444a745d865096bb2
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-01 17:34:33 +0000

    added post author and time
    user can see post author and time
    user can see newest post first
    changed controller to return all informations

--- recipebookbook-frontend/src/components/Post.js
+++ recipebookbook-frontend/src/components/Post.js
@@ -1,13 +1,12 @@
 import React from 'react';
-const Post = ({ post, onRemovePost = f => f, editingPost = f => f }) => (
-  <div className="single-post" key={post.id}>
-    <h4>
-      {post.message} - {post.id}
-    </h4>
-    <button onClick={() => onRemovePost(post.id)}>Erase</button>
-    <button onClick={() => editingPost(post.id)}>Edit</button>
+import Moment from 'react-moment';
+const Post = ({post, onRemovePost=f=>f, editingPost=f=>f}) =>
+    <div className="single-post" key={post.id}>
+        <strong>{post.user.email}</strong><h4> {post.message}</h4>
+       <Moment format="YYYY-MM-DD - HH:mm">{post.created_at}</Moment>
+       <button onClick={() => onRemovePost(post.id)}>Erase</button>
+   <button onClick={() => editingPost(post.id)}>Edit</button>
 
-    <hr />
-  </div>
-);
+   <hr />
+    </div>
 export default Post;
--- recipebookbook-frontend/src/components/PostsContainer.js
+++ recipebookbook-frontend/src/components/PostsContainer.js
@@ -5,105 +5,103 @@ import NewPostForm from './NewPostForm';
 import EditPostForm from './EditPostForm';
 
 class PostsContainer extends Component {
-  constructor(props) {
-    super(props);
+
+  constructor(props){
+    super(props)
     this.state = {
       posts: [],
       editingPostId: null
-    };
-    this.addNewPost = this.addNewPost.bind(this);
-    this.removePost = this.onRemovePost.bind(this);
-    this.editingPost = this.editingPost.bind(this);
-    this.editPost = this.editPost.bind(this);
+    }
+    this.addNewPost = this.addNewPost.bind(this)
+    this.removePost = this.onRemovePost.bind(this)
+    this.editingPost = this.editingPost.bind(this)
+    this.editPost = this.editPost.bind(this)
   }
 
   componentDidMount() {
-    axios
-      .get('http://localhost:3001/api/v1/posts')
-      .then(response => {
-        console.log(response);
-        this.setState({
-          posts: response.data
-        });
+    axios.get('http://localhost:3001/api/v1/posts')
+    .then(response => {
+      console.log(response)
+      this.setState({
+        posts: response.data
       })
-      .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error))
   }
 
   addNewPost(message) {
-    axios
-      .post('/api/v1/posts', { post: { message } })
-      .then(response => {
-        console.log(response);
-        const posts = [...this.state.posts, response.data];
-        this.setState({ posts });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    axios.post( '/api/v1/posts', { post: {message} })
+    .then(response => {
+        const posts = [ response.data, ...this.state.posts  ]
+        this.setState({posts})
+    })
+    .catch(error => {
+        console.log(error)
+    })
   }
 
   editingPost(id) {
-    this.setState({
-      editingPostId: id
-    });
-  }
+        this.setState({
+            editingPostId: id
+        })
+    }
 
-  editPost(id, message) {
-    axios
-      .put('/api/v1/posts/' + id, {
+editPost(id, message) {
+    axios.put( '/api/v1/posts/' + id, {
         post: {
-          message
+            message
         }
-      })
-      .then(response => {
-        console.log(response);
+    })
+    .then(response => {
         const posts = this.state.posts;
-        posts[id - 1] = { id, message };
+        posts.forEach(function(post) {
+          if (post.id === id) {
+            post.message = message
+          }
+        })
         this.setState(() => ({
-          posts,
-          editingPostId: null
-        }));
-      })
-      .catch(error => console.log(error));
-  }
+            posts,
+            editingPostId: null
+        }))
+    })
+    .catch(error => console.log(error));
+}
 
   onRemovePost(id) {
-    console.log(`deleting post ${id}`);
-    axios
-      .delete('http://localhost:3001/api/v1/posts/' + id)
-      .then(response => {
-        const posts = this.state.posts.filter(post => post.id !== id);
-        this.setState({ posts });
-      })
-      .catch(error => console.log(error));
+    axios.delete( '/api/v1/posts/' + id )
+    .then(response => {
+        const posts = this.state.posts.filter(
+            post => post.id !== id
+        )
+        this.setState({posts})
+    })
+    .catch(error => console.log(error))
   }
+
 
   render() {
     return (
       <div className="Posts-container">
-        <NewPostForm onNewPost={this.addNewPost} />
-        {this.state.posts.map(post => {
-          if (this.state.editingPost === post.id) {
-            return (
-              <EditPostForm
-                post={post}
-                key={post.id}
-                editPost={this.editPost}
-              />
-            );
+
+        <NewPostForm onNewPost={this.addNewPost}/>
+        {this.state.posts.map( post => {
+          if (this.state.editingPostId === post.id) {
+              return (<EditPostForm
+                        post={post}
+                        key={post.id}
+                        editPost={this.editPost}
+                  />)
           } else {
-            return (
-              <Post
+              return (<Post
                 post={post}
                 key={post.id}
                 onRemovePost={this.removePost}
-                editingPost={this.editPost}
-              />
-            );
-          }
-        })}
+                editingPost={this.editingPost}
+              />)
+            }
+          })}
       </div>
-    );
+    )
   }
 }
 
