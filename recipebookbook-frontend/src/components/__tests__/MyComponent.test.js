fac9239476f235a877368cd1029aa1169b8721f6
James Rodney <jlrodney95@gmail.com>
2018-11-02 08:53:08 +0000

    Tested for an add post button - Starting to test that the button causes a method - remember the difference between shallow and mount

--- recipebookbook-frontend/src/components/__tests__/MyComponent.test.js
+++ recipebookbook-frontend/src/components/__tests__/MyComponent.test.js
@@ -1,9 +1,11 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import  App from '../../App.js';
 import axios from 'axios';
-import PostContainer from '../PostsContainer.js'
+import PostsContainer from '../PostsContainer.js'
+import NewPostForm from '../NewPostForm.js'
 
 describe('should render title', () => {
 
@@ -15,7 +17,7 @@ describe('should render title', () => {
 
   it('should have title', () => {
     const component = shallow(<App/>);
-    var node = component.find('h1')
+    const node = component.find('h1')
     expect(node.hasClass('App-title')).toBeTruthy();
     expect(node.html()).toEqual('<h1 class="App-title">RecipeBookBook!</h1>')
   });
@@ -25,17 +27,28 @@ describe('should render posts', () => {
   it('should fetch a post', () => {
     const getSpy = jest.spyOn(axios, 'get');
     const postInstance = shallow(
-      <PostContainer/>
+      <PostsContainer/>
     );
     expect(getSpy).toBeCalled();
   })
 
+  it('should have an add post button', () => {
+    const wrapper = shallow(<NewPostForm />);
+    // console.log("asdfj;alsdkjf;lakjsdf;ljkasddd;fjl;asd");
+    // console.log(wrapper.debug());
+    const node = wrapper.find("button")
+    expect(node.hasClass('submess')).toBeTruthy();
+    // expect(wrapper.contains(<div />)).toBeTruthy();
+  })
+
   it('can add post', () => {
     const message = 'potato'
-    const component = shallow(<App/>);
-    var node = component.find('.message').setValue(message)
-    component.find('.submess').onClick()
-    const actual = component.find('.Posts-container').getText()
+    const component = shallow(<NewPostForm />);
+    var node = component.find('.message')
+    node.value = message
+
+    component.find('.submess').simulate('click')
     expect(actual).to.equal(message)
   })
+
 })
